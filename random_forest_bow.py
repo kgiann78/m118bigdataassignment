5b21113ba19c842fba371b3492d6aca1bd7da7f0
ppetrou <petrakisp13@gmail.com>
2016-11-27 18:13:15 +0200

    complete metric.auc and roc plot for random forest and svm

--- random_forest_bow.py
+++ random_forest_bow.py
@@ -108,4 +108,4 @@ plt.ylabel('True Positive Rate')
 plt.title('Receiver operating characteristic example')
 plt.legend(loc="lower right")
 
-plt.savefig('random_forest_svd_roc.png')
+plt.savefig('random_forest_bow_roc.png')
